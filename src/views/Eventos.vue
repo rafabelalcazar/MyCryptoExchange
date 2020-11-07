135a506c72b1ac8b0b10a977b3580fb354cb7a6a
rafabelalcazar <rafaelb@unicauca.edu.co>
2020-11-06 20:18:47 -0500

    FEAT: View Eventos

---
+++ src/views/Eventos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="about">
+      <h1>Eventos</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Eventos",
+};
+</script>
+
+<style scoped>
+</style>
